fd6b375876afdb3a26f07c7d2b1ae4b33fbd2539
Christian <bsogteco@gmail.com>
2022-10-03 11:49:17 -0700

    Update some simple codes
    codes that I'm learning and practicing

---
+++ csv/main.py
@@ -0,0 +1,17 @@
+#with open("weather_data.csv") as data_file:
+#    data = data_file.readlines()
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print (type(data))
